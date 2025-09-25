72502dd653b7e2196334396781dec3e78a12ac10
Ahmad Alfaridzi <Ahmadalfaridzi5@gmail.com>
2025-09-25 09:48:11 +0700

    update dashboard

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,3 +1,15 @@
-import DashboardPage from "@/module/dashboard/DashboardPage";
+import Breadcrumbs from "@/components/ui/breadcrumb";
+import DashboardClient from "@/module/dashboard/DashboardPage";
 
-export default DashboardPage;
+export default function DashboardPage() {
+  return (
+    <div className="w-full min-h-screen overflow-x-hidden px-2 pb-6">
+      <div className="max-w-screen-2xl mx-auto">
+        <div className="p-4 font-semibold">
+          <Breadcrumbs />
+        </div>
+        <DashboardClient />
+      </div>
+    </div>
+  );
+}
